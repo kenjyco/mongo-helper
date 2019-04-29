1fcf4e03cf2608736cfd65d23f53a4e4ea220723
Ken <kenjyco@gmail.com>
2019-04-29 02:58:10 -0500

    Bump to v0.0.5

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='mongo-helper',
-    version='0.0.4',
+    version='0.0.5',
     description='Helper funcs and tools for working with MongoDB',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/mongo-helper',
-    download_url='https://github.com/kenjyco/mongo-helper/tarball/v0.0.4',
+    download_url='https://github.com/kenjyco/mongo-helper/tarball/v0.0.5',
     packages=find_packages(),
     install_requires=[
         'pymongo==3.7.2',
