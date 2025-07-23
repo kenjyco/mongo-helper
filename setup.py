b42d142a4e8a232cfd8e3b102f319dda360b7e27
Ken <kenjyco@gmail.com>
2025-07-23 14:20:03 -0500

    Bump to v0.1.1

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open('requirements.txt', 'r') as fp:
 
 setup(
     name='mongo-helper',
-    version='0.1.0',
+    version='0.1.1',
     description='Helper funcs and tools for working with MongoDB',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/mongo-helper',
-    download_url='https://github.com/kenjyco/mongo-helper/tarball/v0.1.0',
+    download_url='https://github.com/kenjyco/mongo-helper/tarball/v0.1.1',
     packages=find_packages(),
     # setup_requires=['pytest-runner'],
     # tests_require=['pytest'],
